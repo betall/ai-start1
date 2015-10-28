249027b2916d05dcb8b070e5fbb9b6c3765ff407
betall <001vk001@gmail.com>
2015-10-28 17:45:23 +0200

    work update2

--- src/core/agent/impl/SimpleEnvironmentView.java
+++ src/core/agent/impl/SimpleEnvironmentView.java
@@ -1,7 +1,21 @@
 package core.agent.impl;
 
+import core.agent.Agent;
+import core.agent.EnvironmentView;
+
 /**
  * Created by 001vk on 28.10.2015.
  */
-public class SimpleEnvironmentView {
+public class SimpleEnvironmentView implements EnvironmentView{
+    @Override
+    public void agentActed(Agent agent, Action action,
+                           EnvironmentState resultingState){
+        System.out.println("Agent acted: "+action.toString());
+    }
+
+    @Override
+    public void agentAdded(Agent agent, EnvironmentState resultingState){ System.out.println("Agent added.");}
+
+    @Override
+    public void notify(String msg) { System.out.println("Message: "+msg);}
 }
